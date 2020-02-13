e6a255571ffb076be78793aed6a48d67f42557c4
Louis Wollenberger <lvwmailbox@yahoo.com>
2020-02-12 17:54:31 -0800

    Refactor image loading to use Picasso

--- app/src/main/java/com/example/android/restful/MyAdapter.java
+++ app/src/main/java/com/example/android/restful/MyAdapter.java
@@ -1,9 +1,6 @@
 package com.example.android.restful;
 
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.os.AsyncTask;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,17 +11,21 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.android.restful.model.DataItem;
-import com.example.android.restful.utils.ImageCacheManager;
+import com.squareup.picasso.Picasso;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
 import java.util.List;
+
+/**
+ * Includes Picasso for retrieving and storing images for loading in into the recyclerview
+ */
 
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
 
     private List<DataItem> mItems;
     private Context mContext;
+
+    public static final String PHOTOS_BASE_URL =
+            "http://560057.youcanlearnit.net/services/images/";
 
     //store and persist each image for the lifetime of the activity
     //private Map<String, Bitmap> mBitmaps = new HashMap<>();
@@ -50,14 +51,11 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
         try {
             holder.tvView.setText(item.getItemName());
 
-            Bitmap bitmap = ImageCacheManager.getBitmap(mContext, item);
-            if (bitmap == null) {
-                ImageDownloaderTask task = new ImageDownloaderTask();
-                task.setViewHolder(holder);
-                task.execute(item);
-            } else {
-               holder.imageView.setImageBitmap(bitmap);
-            }
+            String url = PHOTOS_BASE_URL + item.getImage();
+
+            //Download image on background thread, cache the image, and display the image
+            Picasso.get().load(url).resize(50, 50).into(holder.imageView);
+
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -77,52 +75,6 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
             super(itemView);
             tvView = itemView.findViewById(R.id.textView);
             imageView = itemView.findViewById(R.id.imageView);
-        }
-    }
-
-    /**
-     * Each time an image is needed, execute the AsyncTask, then save it to the app
-     */
-    private class ImageDownloaderTask extends AsyncTask <DataItem, Void, Bitmap> {
-
-        public static final String PHOTOS_BASE_URL =
-                "http://560057.youcanlearnit.net/services/images/";
-        private DataItem mDataItem;
-        private MyAdapter.ViewHolder mHolder;   //Reference to the current row
-
-        public void setViewHolder (MyAdapter.ViewHolder holder) {
-            mHolder = holder;
-        }
-
-        @Override
-        protected Bitmap doInBackground(DataItem... dataItems) {
-            mDataItem = dataItems[0];
-            String imageUrl = PHOTOS_BASE_URL + mDataItem.getImage();
-            InputStream inputStream = null;
-
-            try {
-                //Download and return the Bitmap
-                inputStream = (InputStream) new URL(imageUrl).getContent();
-                return BitmapFactory.decodeStream(inputStream);
-            } catch (IOException e) {
-                e.printStackTrace();
-            } finally {
-                try {
-                    if (inputStream != null) {
-                        inputStream.close();
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(Bitmap bitmap) {
-            super.onPostExecute(bitmap);
-            mHolder.imageView.setImageBitmap(bitmap);   //Attach the image to the view
-            ImageCacheManager.putBitmap(mContext, mDataItem, bitmap);
         }
     }
 }
